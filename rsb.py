7edae0b1d369e9ce18aaaa20af258dd2e451ced2
Filippo Biondi <filippo22@ru.is>
2025-02-08 20:55:00 +0100

    updated rsb

--- rsb.py
+++ rsb.py
@@ -9,7 +9,8 @@ result = cur.fetchall()
 
 for r in result:
     print(dict(r))
-    os.system(f"./syns {r['nodes']} {r['p']}")
+    os.system(f"cp synthetic-hg/graph_{r['nodes']}_{int(r['p']*100)}_{r['degree']}.txt graph.txt")
+    os.system(f"cp synthetic-hg/partition_{r['nodes']}_{int(r['p']*100)}_{r['degree']}.txt partition.txt")
     os.system("timeout 7200 python berun.py")
     os.system("pkill java")
     try:
@@ -24,3 +25,5 @@ for r in result:
         print(r['name'], "TIMEOUT")
         cur.execute("UPDATE results SET time = ?, result = ? where name= ?", (7200, r['nodes'], r['name']))
         con.commit()
+    os.system("rm graph.txt")
+    os.system("rm partition.txt")
