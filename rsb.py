fa1882168c8169335017b9a7c2f12f7e68db2b02
Filippo Biondi <filippo22@ru.is>
2025-02-19 15:19:03 +0100

    new valmari bench

--- rsb.py
+++ rsb.py
@@ -7,12 +7,24 @@ cur = con.cursor()
 cur.execute('select * from results where result = -1 order by nodes;')
 result = cur.fetchall()
 
+def adapt():
+    nodes = 0
+    lines = []
+    with open("graph.txt") as f:
+        lines = f.read().split("\n")[:-1]
+        nodes = int(lines[0])
+    with open("graph.txt", "w") as f:
+        f.write("\n".join(lines[2:]))
+    return nodes
+
+
 for r in result:
     print(dict(r))
     #os.system(f"./syns {r['nodes']} {r['p']} {r['degree']}")
     os.system(f"./tunget {r['name']}")
+    nodes = adapt()
 
-    os.system("timeout 10800 python3 berun.py")
+    os.system("timeout 10800 java -jar epsBE.jar graph.txt 131  0 0 1 res.txt")
     os.system("pkill java")
     try:
         f = open("res.txt", "r")
