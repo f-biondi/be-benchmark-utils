617c97e9b94d92b4b5c3bfac30a90285104db5c4
Filippo Biondi <filippo22@ru.is>
2025-02-19 15:30:21 +0100

    updated valmari bench

--- valmarib.py
+++ valmarib.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+from datetime import datetime
 
 con = sqlite3.connect("sresult.db")
 con.row_factory = sqlite3.Row
@@ -17,24 +18,26 @@ def adapt():
         f.write("\n".join(lines[2:]))
     return nodes
 
-
 for r in result:
     print(dict(r))
     #os.system(f"./syns {r['nodes']} {r['p']} {r['degree']}")
     os.system(f"./tunget {r['name']}")
-    #nodes = adapt()
+    nodes = adapt()
 
-    os.system("timeout 10800 java -jar epsBE.jar graph.txt 131  0 0 1 res.txt")
+    start = datetime.now()
+    os.system(f"timeout 10800 java -jar epsBE.jar graph.txt {nodes} 0 0 1 res.txt")
+    end = datetime.now()
     os.system("pkill java")
     try:
         f = open("res.txt", "r")
         l = f.read().split("\n")
         f.close()
-        print(r['name'], l[1], l[0])
-        cur.execute("UPDATE results SET time = ?, result = ? where name= ?", (l[1], l[0], r['name']))
+        time = (end - start).total_seconds()
+        result = len(l[1].split(",")) 
+        cur.execute("UPDATE results SET time = ?, result = ? where name= ?", (time, result, r['name']))
         con.commit()
         os.system("rm res.txt")
-    except FileNotFoundError:
+    except Exception:
         print(r['name'], "TIMEOUT")
         cur.execute("UPDATE results SET time = ?, result = ? where name= ?", (10800, r['nodes'], r['name']))
         con.commit()
